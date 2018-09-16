4414506e87b4ee3d0c57546ae2c0743a91173f41
ShadowX <32039975+ShadwX57@users.noreply.github.com>
2018-09-16 10:28:01 +0300

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
+  if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Sorry pal, you can't do that.");
   if(args[0] == "help"){
     message.reply("Usage: +removerole <user> <role>");
     return;
