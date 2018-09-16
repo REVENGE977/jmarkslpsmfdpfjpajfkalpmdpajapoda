f3bededf3f5b4ddb84caf3ac0a1fe64e7f8c88dd
ShadowX <32039975+ShadwX57@users.noreply.github.com>
2018-09-16 10:04:00 +0300

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   //!addrole @andrew Dog Person
   if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
   if(args[0] == "help"){
-    message.reply("Usage: !addrole <user> <role>");
+    message.reply("Usage: +addrole <user> <role>");
     return;
   }
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
